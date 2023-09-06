207902a3fd1d9adae7a595375b82cc2fc5434a5c
pvitorsouza <vitin.7@icloud.com>
2023-09-06 17:45:33 -0300

    first commit

---
+++ Anuncio.Presentation/Controlle/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Anuncio.Presentation.Controlle
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
